16bcef9fe43348e0bf74a4770b91b71447f840aa
Dmitriy Aratin <dima.aratin@mail.ru>
2022-12-31 13:21:27 +0700

    Добавлены поля updated и deleted для работы с объектом Employees

--- product_app/serializers/employees/UpdateEmployeesSerializer.py
+++ product_app/serializers/employees/UpdateEmployeesSerializer.py
@@ -1,18 +1,23 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from product_app.models.EmployeesModel import EmployeesModel
 
 
 class UpdateEmployeesSerializer(serializers.ModelSerializer):
+
+    login = serializers.CharField(required=False)
+
     class Meta:
         model = EmployeesModel
-        fields = "__all__"
+        exclude = ("updated", "deleted")
 
     def validate(self, attrs):
         if (
             EmployeesModel.objects.exclude(id=self.instance.id)
             .filter(
-                login=self.initial_data["login"],
+                login=attrs.get("login", self.instance.login),
+                deleted=None,
             )
             .count()
             != 0
@@ -26,6 +31,7 @@ class UpdateEmployeesSerializer(serializers.ModelSerializer):
         instance.full_name = validated_data.get("full_name", instance.full_name)
         instance.is_developer = validated_data.get("is_developer", instance.is_developer)
         instance.is_manager = validated_data.get("is_manager", instance.is_manager)
+        instance.updated = timezone.now()
         instance.save()
 
         return instance
