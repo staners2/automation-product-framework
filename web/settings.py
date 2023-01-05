d5e75c15e8378882a38022ee55e8551637c1f652
Dmitriy Aratin <dima.aratin0@gmail.com>
2023-01-05 21:47:06 +0700

    chore: Добавлены parser's для различных форматов запросов

--- web/settings.py
+++ web/settings.py
@@ -63,7 +63,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Allow only json response
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
-    "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
+    "DEFAULT_PARSER_CLASSES": (
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework.authentication.BasicAuthentication',
